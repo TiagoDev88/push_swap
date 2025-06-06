b215f9fa1cd04aec6572bb94ad9ae066dd6e70a8
TiagoDev88 <tiagomonteiro.dev@gmail.com>
2025-06-06 23:26:57 +0100

    with print stacks for debug

--- src/main_tester.c
+++ src/main_tester.c
@@ -1,4 +1,5 @@
 #include "../includes/push_swap.h"
+#include <stdio.h>
 
 static void print_stack(t_node *stack, char *name)
 {
@@ -14,7 +15,36 @@ static void print_stack(t_node *stack, char *name)
     ft_printf("------\n");
 }
 
+static void print_stacks_side_by_side(t_node *stack_a, t_node *stack_b)
+{
+	t_node *node_a = stack_a ? stack_a : NULL;
+	t_node *node_b = stack_b ? stack_b : NULL;
 
+	printf(" Index | Stack A | Stack B\n");
+	printf("-------|---------|--------\n");
+
+	int i = 0;
+	while (node_a || node_b)
+	{
+		printf(" [%3d] | ", i++);
+
+		if (node_a)
+		{
+			printf("%7d | ", node_a->value);
+			node_a = node_a->next;
+		}
+		else
+			printf("%7s | ", "---");
+
+		if (node_b)
+		{
+			printf("%7d\n", node_b->value);
+			node_b = node_b->next;
+		}
+		else
+			printf("%7s\n", "---");
+	}
+}
 
 int	main(int argc, char **argv)
 {
@@ -32,6 +62,7 @@ int	main(int argc, char **argv)
 	if (is_order(stack_a))
 		return (0);
 	push_swap(&stack_a, &stack_b);
+	print_stacks_side_by_side(stack_a, stack_b);
 	print_stack(stack_a, "A");
 	free_stack(&stack_a);
 	free_stack(&stack_b);
